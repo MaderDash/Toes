c4e97ba564eb785122845808c1f3d0dcc890bcd0
Maderdash <supperslash@gmail.com>
2024-06-30 13:10:42 -0500

    Updating for the day.

--- main.ino
+++ main.ino
@@ -17,8 +17,7 @@ byte matrex[4][16] = {
   {21,22,23,24,25,26,27, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Flat
   {40,42,30,31,32,36,48, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Smile
   { 0, 6, 8,12,16,17,18, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Slow
-  { 2, 3, 4, 8,12,14,20,21,27,28,34,36,40,44,45,46} // Stop  
- 
+  { 2, 3, 4, 8,12,14,20,21,27,28,34,36,40,44,45,46}  // Stop
 };
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
@@ -26,7 +25,7 @@ void setup() {
 
 void test() {
   for (int counter = 0; counter < NUM_LEDS; counter++) {
-    leds[counter] = CRGB::Red;  // Set the first LED to red
+    leds[counter] = CRGB::Red;  // Set the first LED to red 
     FastLED.show();
     delay(100);
     leds[counter] = CRGB::Black;  // Set the first LED to black
